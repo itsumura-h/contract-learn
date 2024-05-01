932930f750b42dd8ccb4a0e55c90753d42aa0288
itsumura-h <dumblepy@gmail.com>
2024-05-01 09:04:23 +0000

    add ProxyCoin

--- src/scripts/Coin/transferMinterToAlice.ts
+++ src/scripts/Coin/transferMinterToAlice.ts
@@ -1,11 +1,10 @@
 import { parseEther, createWalletClient, http } from "viem";
-import {minter, alice} from "./libs/accounts"
+import {minter, alice} from "../libs/accounts"
 import hre from "hardhat";
 
 async function main() {
   const initialSupply = parseEther("1000000");
   const coin = await hre.viem.deployContract("Coin", [initialSupply]);
-  console.log(coin);
 
   let balance = await coin.read.balanceOf([minter.account.address]);
   console.log("Minter balance:", balance.toString());
