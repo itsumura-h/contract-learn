907e367d9ac9901c2bf3df9d65b3264f6e96e581
itsumura-h <dumblepy@gmail.com>
2024-06-20 05:53:06 +0000

    wip

--- src/contracts/ProxyCoin/FiatTokenV1.sol
+++ src/contracts/ProxyCoin/FiatTokenV1.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract FiatTokenV1 is ERC20, UUPSUpgradeable, Ownable, Initializable {
+contract FiatTokenV1 is ERC20Upgradeable, UUPSUpgradeable, Ownable {
     constructor(uint256 initialSupply) ERC20("JPYC_v1", "JPYC") { // name, symbol
         _mint(msg.sender, initialSupply);
     }
--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-foundry";
-import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
